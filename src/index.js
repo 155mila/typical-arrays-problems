c7ce1f85e1d853c48b803cb1cb50ae8f08b2d8b8
155mila <58401380+155mila@users.noreply.github.com>
2020-03-22 18:11:08 +0300

    find error

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 
 exports.min = function min (array) {
-    if(array === undefined || array === 0){
+    if(array === undefined || array.length === 0){
         return 0;
     }
     return Math.min.apply(null, array);
@@ -8,14 +8,14 @@ exports.min = function min (array) {
 }
 
 exports.max = function max (array) {
-    if(array === undefined || array === 0){
+    if(array === undefined || array.length === 0){
         return 0;
     }
     return Math.max.apply(null, array);
 }
 
 exports.avg = function avg (array) {
-    if(array === undefined || array === 0){
+    if(array === undefined || array.length === 0){
         return 0;
     }
   let sum = array.reduce((a, b) => a + b, 0);
